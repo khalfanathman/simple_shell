b388bf537058573fa425a0f9bb87450264bc1845
Khalfan Athman <khalfanathman12@gmail.com>
2023-05-21 01:35:58 +0300

    adding freeing memory function

--- terminal_handler.c
+++ terminal_handler.c
@@ -16,4 +16,24 @@ void sigint_handler(int sig)
 		_exit(0);
 	}
 }
+/**
+ * cleanup - clean up the varable used before next iteration.
+ * @shell: Pointer to the shell structure to be cleaned.
+ */
+void cleanup(shell_var *shell)
+{
+	shell_var *sh = shell;
 
+	free(sh->buf);
+	free(sh->fin);
+	free(sh->finArr);
+	sh->chRead = 0;
+	shell->num_tokens = 0;
+	sh->command = NULL;
+	sh->num_tokens = 0;
+	sh->size = 0;
+	sh->finArr = NULL;
+	sh->buf = NULL;
+	sh->fin = NULL;
+	fflush(stdin);
+}
