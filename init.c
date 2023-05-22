1c751f5e6ea8a5b2433169e119398b7a58dbbfa6
Khalfan Athman <khalfanathman12@gmail.com>
2023-05-22 16:35:58 +0300

    initializing more struct variables

--- init.c
+++ init.c
@@ -2,18 +2,19 @@
 /**
  * initialize_shell - Initializes the shell structure.
  * @shell: Pointer to the shell structure.
+ * @envp: environment variables array
  */
-void initialize_shell(shell_var *shell)
+void initialize_shell(shell_var *shell, char **envp)
 {
-	/* Initialize the shell structure */
 	shell->PROMPT = true;
 	shell->buf = NULL;
 	shell->size = 0;
 	shell->chRead = 0;
 	shell->finArr = NULL;
 	shell->fin = NULL;
+	shell->environs = envp;
+	shell->PATH = _getenv("PATH", envp);
 	shell->command = NULL;
 	shell->process_id = 1;
 	shell->num_tokens = 0;
-	/* Initialize other variables as needed */
 }
