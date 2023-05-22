dc923ce6f82348c37427f38d78f26c63225fc045
Khalfan Athman <khalfanathman12@gmail.com>
2023-05-22 16:33:41 +0300

    adding styling

--- command.c
+++ command.c
@@ -30,27 +30,27 @@ char *conc_fpath(char **filepath, char *path_entry, char *cmd)
 
 /**
  * check_cmd_exist - Checks if a cmd exists in the PATH environment variable.
- * @term_cm: Command to check.
  *
+ * @term_cm: Command to check.
+ * @shell: The pointer to struct variables to execute.
  * Return: The command if it exists, NULL otherwise.
  */
 
 char *check_cmd_exist(shell_var *shell, char *term_cm)
 {
 	shell_var *sh = shell;
-	char *copy = NULL, *path = getenv("PATH");
+	char *copy = NULL;
 	int i = 0;
 	char *fpath = malloc(sizeof(char *) * 11);
 	char **arr, *commnd;
 
-	if (path == NULL || fpath == NULL)
+	if (sh->PATH == NULL || fpath == NULL)
 	{
 		perror("path");
 		exit(EXIT_FAILURE);
 	}
-
-	copy = copy_str(copy, path);
-	arr = set_array_cmd(sh,&copy, 10);
+	copy = copy_str(copy, sh->PATH);
+	arr = set_array_cmd(sh, &copy, 10);
 	array_sort(arr, 10);
 
 	while (arr[i] != NULL)
@@ -88,6 +88,7 @@ void execute_command(shell_var *shell, char **fin, char **envp)
 {
 	pid_t child_pid;
 	shell_var *sh = shell;
+
 	child_pid = fork();
 
 	if (child_pid == 0)
@@ -104,8 +105,33 @@ void execute_command(shell_var *shell, char **fin, char **envp)
 	{
 		wait(NULL);
 		sh->command = NULL;
-		free(fin);
+		/* free(shell->fin); */
 		(sh->process_id)++;
 		return;
 	}
+}
+/**
+ * control_d - Exits a command using CTR + D.
+ * @shell: The pointer to struct variables.
+ *
+ * @envp: The array of environment variables.
+ */
+void control_d(shell_var *shell, char **envp)
+{
+	shell_var *sh = shell;
+
+	if (sh->buf != NULL)
+	{
+		sh->fin = setArray(sh, &(sh->buf), sh->size);
+		if ((sh->fin)[0] != NULL)
+		{
+			sh->command = check_cmd_exist(sh, (sh->fin)[0]);
+		}
+		if (sh->command != NULL)
+		{
+			execute_command(sh, sh->fin, envp);
+		}
+		cleanup(sh);
+	}
+	exit(EXIT_SUCCESS);
 }
